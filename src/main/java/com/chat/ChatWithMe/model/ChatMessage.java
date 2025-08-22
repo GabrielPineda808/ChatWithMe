42cf5ae572d588c1e97b3b39ce9daa44ffc18869
Gabe <gabepineda6@gmail.com>
2025-08-22 12:03:42 -0700

    removed lombok annotations causing error parsing through the payload

--- src/main/java/com/chat/ChatWithMe/model/ChatMessage.java
+++ src/main/java/com/chat/ChatWithMe/model/ChatMessage.java
@@ -1,12 +1,16 @@
+// com/chat/ChatWithMe/model/ChatMessage.java
 package com.chat.ChatWithMe.model;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
 public class ChatMessage {
-    private long id;
     private String sender;
     private String content;
+
+    public ChatMessage() {}                   // required
+    public ChatMessage(String sender, String content) {
+        this.sender = sender; this.content = content;
+    }
+    public String getSender()  { return sender;  }
+    public void setSender(String s) { this.sender = s; }
+    public String getContent() { return content; }
+    public void setContent(String c) { this.content = c; }
 }
