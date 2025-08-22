d9a4b3c1a61a73fcf04ad2c741426d61754d52e4
Gabe <gabepineda6@gmail.com>
2025-08-21 23:10:39 -0700

    controller to direct live chats. Two end points one that will send messages and the other to recieve messages from that end point

--- src/main/java/com/chat/ChatWithMe/controller/ChatController.java
+++ src/main/java/com/chat/ChatWithMe/controller/ChatController.java
@@ -1,4 +1,24 @@
 package com.chat.ChatWithMe.controller;
 
+import com.chat.ChatWithMe.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ChatController {
+
+    @MessageMapping("/sendmessage")
+    //maps web socket messages to its destination. When someone sends a message to /sendmessage endpoint it will send that
+    // message to whichever endpoint you write in the @sendto annotation
+    @SendTo("/topic/messages") //recieves messages from /topic/messages
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+
+    @GetMapping("chat")
+    public String chat(){
+        return "chat";
+    }
 }
